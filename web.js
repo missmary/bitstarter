c030814489782615f9cf2eaa05378d90b00a2ccd
Kasia Siudek <katarzyna.siudek@gmail.com>
2013-07-21 11:55:26 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+//var buf = new Buffer(256);
+//var outfile = "hello.txt";
+//var out = "A startup is a business built to grow rapidly\n";
+//fs.writeFileSync(outfile,out);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+var buf = new Buffer(fs.readFileSync('index.html'));
+response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
