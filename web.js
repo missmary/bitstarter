238db16087b3232742613040cdc49dde5153b463
Kasia Siudek <katarzyna.siudek@gmail.com>
2013-07-21 12:01:56 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hellorld2!');
 //var buf = new Buffer(fs.readFileSync('index.html'));
 //response.send(buf.toString());
 });
